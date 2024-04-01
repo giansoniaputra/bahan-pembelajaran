59b7d315a63b78f13269f520220d748c2351c79b
Gian Sonia <109233308+giansoniaputra@users.noreply.github.com>
2024-04-01 20:33:29 +0700

    First Commit

---
+++ html_dalam_php.php
@@ -0,0 +1,5 @@
+<?php 
+
+echo "<h1>Hai, Nama saya Dewi Septiani</h1>";
+
+?>
---
+++ php_dalam_html.php
@@ -0,0 +1,15 @@
+<!-- PHP DALAM HTML -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP DALAM HTML</title>
+</head>
+<body>
+    <h1>
+        <!-- Baris berikut merupakan contoh penulisan php dalam html -->
+        Hai, Nama Saya <?php echo 'Dewi Septiani' ?>;
+    </h1>
+</body>
+</html>
