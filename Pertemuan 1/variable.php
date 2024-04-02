29cd64b3a6c69b8383f7b44ef074e9ec47bc41b6
giansoniaputra <giansonia555@gmail.com>
2024-04-02 20:45:01 +0700

    Variable

---
+++ Pertemuan 1/variable.php
@@ -0,0 +1,8 @@
+<?php
+// $nama_depan = 'Dewi';
+// $nama_belakang = 'Septiani';
+// $nama_lengkap = "$nama_depan $nama_belakang";
+// $kampus = "UNPAD";
+
+
+// echo "Hai, Nama saya $nama_lengkap dan saya kuliah di $kampus";
--- html_dalam_php.php
+++
@@ -1,5 +0,0 @@
-<?php 
-
-echo "<h1>Hai, Nama saya Dewi Septiani</h1>";
-
-?>
