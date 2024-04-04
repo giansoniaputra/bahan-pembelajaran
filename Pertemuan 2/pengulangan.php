5dccd31d64abbcc3e0e6bf40bdcaf68147d9d410
giansoniaputra <giansonia555@gmail.com>
2024-04-04 20:56:40 +0700

    Perulangan While

--- Pertemuan 2/pengulangan.php
+++ Pertemuan 2/pengulangan.php
@@ -1,6 +1,14 @@
 <?php
+// FOR
+// for ($i = 1; $i <= 5; $i++) {
+//     echo "Hello World!<br>";
+// }
+// echo "Selesai<br>";
 
-for ($i = 1; $i <= 5; $i++) {
-    echo "Hello World!<br>";
-}
-echo "Selesai<br>";
+// WHILE
+
+// $i = 0;
+// while ($i < 5) {
+//     echo "Hello World!<br>";
+//     $i++;
+// }
